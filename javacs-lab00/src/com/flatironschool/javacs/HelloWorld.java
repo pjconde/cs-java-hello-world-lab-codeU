2d6c999c7317ecaf549a572ace24ec137aee21a6
pconde3 <pjconde@gatech.edu>
2016-03-16 14:59:15 -0400

    Finished HelloWorld

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,9 +3,13 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+        String temp = System.getProperty("java.version");
+        Double v = Double.parseDouble(temp.substring(0,3));
+        return v;
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
+        System.out.println(getVersion());
     }
 }
